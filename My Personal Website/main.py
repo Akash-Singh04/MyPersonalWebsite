1a7b065e1010043da395279d908b88659fb37279
Akash Singh <114267538+Akash-Singh04@users.noreply.github.com>
2022-10-12 21:01:54 +0530

    Add files via upload

---
+++ My Personal Website/main.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap4
+app = Flask(__name__)
+bootstrap = Bootstrap4(app)
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route('/blogwebsite')
+def blogwebsite():
+    return render_template("blogwebsite.html")
+
+@app.route('/tinderbot')
+def tinderbot():
+    return render_template("tinder-bot.html")
+
+@app.route('/twitterbot')
+def twitterbot():
+    return render_template("twitter-bot.html")
+
+@app.route('/tindog')
+def tindog():
+    return render_template("tindog.html")
+
+
+if __name__=="__main__":
+    app.run(debug=True)
